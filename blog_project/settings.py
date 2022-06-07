c919a2ee35d6987636d2c1bae32c9e978a81d005
Traniel Pride <tranielmp8@gmail.com>
2022-06-07 14:37:09 -0500

    css static update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -151,8 +151,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/blog_app/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/blog_app/'),
+)
 
 # Custom Settings:
 

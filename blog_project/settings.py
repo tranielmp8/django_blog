65a826ec026fb28505c065f8170ecd98324590c2
Traniel Pride <tranielmp8@gmail.com>
2022-06-07 16:15:35 -0500

    settings update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dnp-django-blog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['dnp-django-blog.herokuapp.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition

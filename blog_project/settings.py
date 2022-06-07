f396207890d732c7febdcfb86c33aa233afe32a9
Traniel Pride <tranielmp8@gmail.com>
2022-06-07 14:01:15 -0500

    added Procfile and requirements

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
